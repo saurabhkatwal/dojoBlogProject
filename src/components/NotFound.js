1fccbfa9c6823937f74e71d3b209d639916e0104
saurabh <saurabhkatwal@gmail.com>
2022-10-27 11:54:22 +0530

    Created a blog project

---
+++ src/components/NotFound.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+const NotFound = () => {
+    return (
+        <div className="not-found">
+            <h2>Sorry</h2>
+            <p>Page not found</p>
+            <Link to="/">Go back to homepage</Link>
+        </div>
+    );
+}
+ 
+export default NotFound;
